c4e1e5c27f780bfeace519396e6f733999d1009e
supriya224 <52038704+supriya224@users.noreply.github.com>
2024-08-15 14:24:57 +0530

    footer completed

--- src/components/shared/footer/Footer.tsx
+++ src/components/shared/footer/Footer.tsx
@@ -1,26 +1,45 @@
 /* eslint-disable react/jsx-no-undef */
-import Image from 'next/image'
-import React from 'react'
-import Logo from "/public/assets/Subtract.png"
+import Image from "next/image";
+import React from "react";
+import Logo from "/public/assets/Subtract.png";
+import { Facebook, Instagram, Linkedin, Twitter } from "react-feather";
 
 const Footer = () => {
   return (
-    <footer className=''>
-        <div>
-            <Image src={Logo}  width={100} height={100} alt=""/>
-            <h3><span>M</span>umair </h3>
-            <ul>
-                <li>Home</li>
-                <li>About Me</li>
-                <li>Services</li>
-                <li>Testimonials</li>
-                <li>Projects</li>
-                <li>Contact</li>
-
-            </ul>
+    <footer className="max-h-screen  ">
+      <div className="bg-[#F8F8F8] py-32 leading-10">
+        <div className="flex items-center justify-center gap-2">
+          <Image src={Logo} width={50} height={50} alt="" />
+          <h3 className="text-2xl">
+            <span className="text-3xl font-bold font-customFont ">M</span>umair{" "}
+          </h3>
         </div>
-    </footer>
-  )
-}
 
-export default Footer
+        <ul className="font-customFont flex justify-center items-center  gap-[12px]">
+          <li>Home</li>
+          <li>About Me</li>
+          <li>Services</li>
+          <li>Testimonials</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+     
+      <div>
+        <div className="flex justify-center items-center  gap-[12px]">
+          <Facebook />
+          <Instagram />
+          <Twitter />
+          <Linkedin />
+        </div>
+      </div>
+      </div>
+      <div className="bg-[#545454] p-3 text-white">
+        <p className="text-center">
+          © 2023 <span className="text-[#FD6F00]">Mumair</span> All Rights Reserved , Inc.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
